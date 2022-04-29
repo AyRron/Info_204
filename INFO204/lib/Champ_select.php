88eb6f4119252206d7d82126360cf61b5d11fa46
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2022-04-29 11:50:16 +0200

    css init3

--- INFO204/lib/Champ_select.php
+++ INFO204/lib/Champ_select.php
@@ -1,7 +1,6 @@
-
-
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>Champ_select</title>
@@ -9,40 +8,35 @@
 </head>
 <body>
 
-<h2 class="top"> Toplane <img src="../image/top_lane.png" /> </h2>
+<div class="top">
+<h2> Toplane <img src="../image/top_lane.png" /> </h2>
+<img src="../image/Aatrox.webp" />
+</div>
 
-<img class="top" src="../image/Aatrox.webp" />
+<div class="jgl">
+<h2 > Jungle <img src="../image/jungle.png" /> </h2>
+<img src="../image/Kayn.webp" />
+</div>
 
+<div class="mid">
+<h2> Midlane <img src="../image/mid_lane.png" /> </h2>
+<img src="../image/Vladimir.webp" />
+</div>
 
+<div class="bot">
+<h2> Botlane <img src="../image/bot_lane.webp" /> </h2>
+<img src="../image/Jinx.webp" />
+</div>
 
-<h2 class="jgl"> Jungle <img src="../image/jungle.png" /> </h2>
+<div class="sup">
+<h2> Support <img src="../image/support.png" /> </h2>
+<img src="../image/Tresh.webp" /></br>
+</div>
 
-<img class="jgl" src="../image/Kayn.webp" />
-
-
-
-<h2 class="mid"> Midlane <img src="../image/mid_lane.png" /> </h2>
-
-<img class="mid" src="../image/Vladimir.webp" />
-
-
-
-<h2 class="bot"> Botlane <img src="../image/bot_lane.webp" /> </h2>
-
-<img class="bot" src="../image/Jinx.webp" />
-
-
-
-<h2 class="sup"> Support <img src="../image/support.png" /> </h2>
-
-<img class="sup" src="../image/Tresh.webp" /></br>
-
-
-    
 </body>
 
 <footer>
 <a href="../index.php" >Retourner a l'acceuil</a></br>
-
 </footer>
+
 </html>
