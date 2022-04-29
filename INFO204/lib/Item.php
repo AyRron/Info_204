1997b3c401c98e45465c68b43a4abe50f0214894
SocrateleVIRIL <maitrea38570@gmail.com>
2022-04-29 12:05:48 +0200

    push
    items adc

--- INFO204/lib/Item.php
+++ INFO204/lib/Item.php
@@ -11,47 +11,89 @@
 <body>
     
 <h2 class="combattant"> Items combattants:</h2>
+<br>
+<h3> Items mythiques </h3>
+
+<img class="combattant" src="../image/items/combattant/Divine_Sunderer_item.webp" />
+<img class="combattant" src="../image/items/combattant/Eclipse_item.webp" />
+<img class="combattant" src="../image/items/combattant/Goredrinker_item.webp" />
+<img class="combattant" src="../image/items/combattant/Infinity_Force_item.webp" />
+<img class="combattant" src="../image/items/combattant/Stridebreaker_item.webp" />
+
+<h3> Items légendaires </h3>
 
 <img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
+<img class="combattant" src="../image/items/combattant/Blade_of_the_Ruined_King_item.webp" />
+<img class="combattant" src="../image/items/combattant/Bloodthirster_item.webp" />
+<img class="combattant" src="../image/items/combattant/Chempunk_Chainsword_item.webp" />
 <img class="combattant" src="../image/items/combattant/Dead_Man_Plate_item.webp" />
 <img class="combattant" src="../image/items/combattant/Death's_Dance_item.webp" />
-<img class="combattant" src="../image/items/combattant/Divine_Sunderer_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
-<img class="combattant" src="../image/items/combattant/Black_Cleaver_item.webp" />
+<img class="combattant" src="../image/items/combattant/Guardian_Angel_item.webp" />
+<img class="combattant" src="../image/items/combattant/Guinsoo_Rageblade_item.webp" />
+<img class="combattant" src="../image/items/combattant/Hullbreaker_item.webp" />
+<img class="combattant" src="../image/items/combattant/Manamune_item.webp" />
+<img class="combattant" src="../image/items/combattant/Maw_of_Malmortius_item.webp" />
+<img class="combattant" src="../image/items/combattant/Ravenous_Hydra_item.webp" />
+<img class="combattant" src="../image/items/combattant/Serylda's_Grudge_item.webp" />
+<img class="combattant" src="../image/items/combattant/Silvermere_Dawn_item.webp" />
+<img class="combattant" src="../image/items/combattant/Sterak's_Gage_item.webp" />
+<img class="combattant" src="../image/items/combattant/Wit's_End_item.webp" />
 
 
 
 <h2 class="tireur"> Items tireurs:</h2>
+<br>
+<h3> Items mythiques </h3>
+
+<img class="combattant" src="../image/items/tireur/Galeforce_item.webp" />
+<img class="combattant" src="../image/items/tireur/Immortal_Shieldbow_item.webp" />
+<img class="combattant" src="../image/items/tireur/Kraken_Slayer_item.webp" />
+
+<h3> Items légendaires </h3>
+
+<img class="combattant" src="../image/items/tireur/Blade_of_the_Ruined_King_item.webp" />
+<img class="combattant" src="../image/items/tireur/Bloodthirster_item.webp" />
+<img class="combattant" src="../image/items/tireur/Essence_Reaver_item.webp" />
+<img class="combattant" src="../image/items/tireur/Guardian_Angel_item.webp" />
+<img class="combattant" src="../image/items/tireur/Guinsoo_Rageblade_item.webp" />
+<img class="combattant" src="../image/items/tireur/Infinity_Edge_item.webp" />
+<img class="combattant" src="../image/items/tireur/Lord_Dominik's_Regards_item.webp" />
+<img class="combattant" src="../image/items/tireur/Manamune_item.webp" />
+<img class="combattant" src="../image/items/tireur/Maw_of_Malmortius_item.webp" />
+<img class="combattant" src="../image/items/tireur/Mercurial_Scimitar_item.webp" />
+<img class="combattant" src="../image/items/tireur/Mortal_Reminder_item.webp" />
+<img class="combattant" src="../image/items/tireur/Navori_Quickblades_item.webp" />
+<img class="combattant" src="../image/items/tireur/Phantom_Dancer_item.webp" />
+<img class="combattant" src="../image/items/tireur/Rapid_Firecannon_item.webp" />
+<img class="combattant" src="../image/items/tireur/Runaan's_Hurricane_item.webp" />
+<img class="combattant" src="../image/items/tireur/Serylda's_Grudge_item.webp" />
+<img class="combattant" src="../image/items/tireur/Stormrazor_item.webp" />
+<img class="combattant" src="../image/items/tireur/The_Collector_item.webp" />
 
 
 
 <h2 class="assassin"> Items assassins:</h2>
-
+<br>
+<h3> Items mythiques </h3>
+<h3> Items légendaires </h3>
 
 
 <h2 class="mage"> Items mages:</h2>
-
+<br>
+<h3> Items mythiques </h3>
+<h3> Items légendaires </h3>
 
 
 <h2 class="tank"> Items tanks:</h2>
-
+<br>
+<h3> Items mythiques </h3>
+<h3> Items légendaires </h3>
 
 
 <h2 class="support"> Items supports:</h2>
+<br>
+<h3> Items mythiques </h3>
+<h3> Items légendaires </h3>
+
 </body>
 </html>
