87b30092fba53120c9920e96f6e373bcd69e92c8
SocrateleVIRIL <maitrea38570@gmail.com>
2022-04-29 11:02:31 +0200

    modif

--- INFO204/index.php
+++ INFO204/index.php
@@ -22,36 +22,6 @@ League of legends
 
 <body>
  
-
-<h2 class="top"> Toplane <img src="image/top_lane.png" /> </h2>
-
-<img class="top" src="image/Aatrox.webp" />
-
-
-
-<h2 class="jgl"> Jungle <img src="image/jungle.png" /> </h2>
-
-<img class="jgl" src="image/Kayn.webp" />
-
-
-
-<h2 class="mid"> Midlane <img src="image/mid_lane.png" /> </h2>
-
-<img class="mid" src="image/Vladimir.webp" />
-
-
-
-<h2 class="bot"> Botlane <img src="image/bot_lane.webp" /> </h2>
-
-<img class="bot" src="image/Jinx.webp" />
-
-
-
-<h2 class="sup"> Support <img src="image/support.png" /> </h2>
-
-<img class="sup" src="image/Tresh.webp" /></br>
-
-
 <a href="lib/Build.php" >Build</a></br>
 <a href="lib/Item.php" >Item</a></br>
 <a href="lib/Rune.php" >Rune</a></br>
--- INFO204/lib/Champ_select.php
+++ INFO204/lib/Champ_select.php
@@ -8,6 +8,36 @@
     <link rel="stylesheet" href="../CSS/style_index.css">
 </head>
 <body>
+
+<h2 class="top"> Toplane <img src="../image/top_lane.png" /> </h2>
+
+<img class="top" src="../image/Aatrox.webp" />
+
+
+
+<h2 class="jgl"> Jungle <img src="../image/jungle.png" /> </h2>
+
+<img class="jgl" src="../image/Kayn.webp" />
+
+
+
+<h2 class="mid"> Midlane <img src="../image/mid_lane.png" /> </h2>
+
+<img class="mid" src="../image/Vladimir.webp" />
+
+
+
+<h2 class="bot"> Botlane <img src="../image/bot_lane.webp" /> </h2>
+
+<img class="bot" src="../image/Jinx.webp" />
+
+
+
+<h2 class="sup"> Support <img src="../image/support.png" /> </h2>
+
+<img class="sup" src="../image/Tresh.webp" /></br>
+
+
     
 </body>
 </html>
