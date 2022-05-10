60a1ab01495a5304b96bb06edb9859060669dabf
AyRron <cyrilpitar@orange.fr>
2022-05-10 14:18:57 +0200

    tableau Build

--- INFO204/lib/Build.php
+++ INFO204/lib/Build.php
@@ -20,10 +20,37 @@
                 print_r($nom);
             }
         }
-        
-        champ_info_nom($conn, $nom);
-        
+        echo("\n");
+        var_dump($champ=champ_info_nom($conn, $nom));
 
+        echo($champ['Nom_Champ']);
+        /*
+        echo("<table>\n");
+        
+        foreach($champ as $key => $value){
+            if($key == "Nom_Champ"){
+                echo("\t<tr>$value</tr>\n");
+
+            } elseif($key == "Rune_f" || $key == "S1"){
+                echo("\t<tr>");
+                if($key == "Rune_f"){
+                    echo("<td>$value</td>");
+                } else{
+                    echo("<td>$value</td></tr>\n");
+                }
+            
+            } elseif($key == "F1" || $key == "S2"){
+                echo("\t<tr>");
+                if($key == "F1"){
+                    echo("<td>$value</td>");
+                } else{
+                    echo("<td>$value</td></tr>\n");
+                }
+
+            }elseif($key == "F2" || $key == "sup1")
+            echo("<h2> clef : $key, valeur : $value</h2>\n");
+        }
+        */
 
         include 'disconnect.php';
     ?>
--- INFO204/lib/Champ_select.php
+++ INFO204/lib/Champ_select.php
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', () => {
          
             const clickedImage = this;
             var nom = this.id;
-            window.location.replace("http://os-vps418.infomaniak.ch:1180/l1_info_3/www/lib/Build.php?id=" + nom);
+            window.location.replace("http://os-vps418.infomaniak.ch:1180/l1_info_3/www_test_C/lib/Build.php?id=" + nom);
              
         })
     ))
