07554d4c8365ee91bc3861e971e1542598c20b20
AyRron <cyrilpitar@orange.fr>
2022-05-13 15:36:22 +0200

    conn

--- INFO204/admin/lib/Rune.crud.php
+++ INFO204/admin/lib/Rune.crud.php
@@ -1,4 +1,10 @@
 <?php
+session_start() ;
+var_dump($_SESSION);
+if(!$_SESSION["admin"]){
+	header("Location: admin_form.php") ; 
+} 
+
 /*---------------------------------------
 CRUD: Gestion des Builds
 ---------------------------------------*/
