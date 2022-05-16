3f7e722b99d9ac8b48a04e460188e69361bcf3e3
SocrateleVIRIL <maitrea38570@gmail.com>
2022-05-16 15:53:31 +0200

    vds

--- INFO204/admin/lib/Items_admin.php
+++ INFO204/admin/lib/Items_admin.php
@@ -102,7 +102,8 @@
 		$core2 = $_POST["F3_mod"];
 		$option1 = $_POST["S1_mod"];
 		$option2 = $_POST["S2_mod"];
-<<<<<<< HEAD
+		
+
 		update_items($conn, $id, $nom, $starting_item, $mythique, $bottes, $core1, $core2, $option1, $option2, $option3);
 =======
 		$option2 = $_POST["S2_mod"];
