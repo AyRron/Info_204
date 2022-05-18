496da8b4aa8469a97cf636c191df8085f40d658a
SocrateleVIRIL <maitrea38570@gmail.com>
2022-05-18 17:00:52 +0200

    runes

--- INFO204/lib/Rune.php
+++ INFO204/lib/Rune.php
@@ -25,11 +25,11 @@
 <body>
 <table id="tab_rune">
     <tr>
-        <th><h2>Précision</h2></th>
-        <th><h2>Domination</h2></th>
-        <th><h2>Sorcellerie</h2></th>
-		<th><h2>Volonté</h2></th>
-        <th><h2>Inspiration</h2></th>
+        <th><h1>Précision</h1></th>
+        <th><h1>Domination</h1></th>
+        <th><h1>Sorcellerie</h1></th>
+		<th><h1>Volonté</h1></th>
+        <th><h1>Inspiration</h1></th>
     </tr> 
     <tr>
         <th>Devenez une légende</th>
@@ -47,7 +47,33 @@
 		<th><img src="../image/runes/Poigne_de_l_immortel.webp" /> <img src="../image/runes/Gardien.webp" /> <img src="../image/runes/Apres_coup.webp" /></th>
         <th><img src="../image/runes/Optimisation_glaciale.webp" /> <img src="../image/runes/Premier_coup.webp" /> <img src="../image/runes/Grimoire_dechaine.webp" /></th>
     </tr>    
+
+
+    <tr>
+        <th><img src="../image/runes/Surcharge_de_pv.webp" /> <img src="../image/runes/Triomphe.webp" /> <img src="../image/runes/Presence_d_esprit.webp" /></th>
+        <th><img src="../image/runes/Coup_bas.webp" /> <img src="../image/runes/Gout_du_sang.webp" /> <img src="../image/runes/Ruee_offensive.webp" /></th>
+        <th><img src="../image/runes/Orbe_anti-magie.webp" /> <img src="../image/runes/Ruban_de_mana.webp" /> <img src="../image/runes/Manteau_nuageux.webp" /></th>
+		<th><img src="../image/runes/Demolition.webp" /> <img src="../image/runes/Fontaine_de_vie.webp" /> <img src="../image/runes/Coup_de_bouclier.webp" /></th>
+        <th><img src="../image/runes/Canaliportation_hextech.webp" /> <img src="../image/runes/Chaussures_magique.webp" /> <img src="../image/runes/Timing_parfait.webp" /></th>
+    </tr>
 	
+
+    <tr>
+        <th><img src="../image/runes/Legende_alacrite.webp" /> <img src="../image/runes/Legende_tenacite.webp" /> <img src="../image/runes/Legende_sangsue.webp" /></th>
+        <th><img src="../image/runes/Balise_zombie.webp" /> <img src="../image/runes/Poro_fantome.webp" /> <img src="../image/runes/Arracheur_d_oeil.webp" /></th>
+        <th><img src="../image/runes/Transcendance.webp" /> <img src="../image/runes/Celerite.webp" /> <img src="../image/runes/Concentration_absolue.webp" /></th>
+		<th><img src="../image/runes/Conditionnement.webp" /> <img src="../image/runes/Second_souffle.webp" /> <img src="../image/runes/Plaque_d_os.webp" /></th>
+        <th><img src="../image/runes/Marche_du_futur.webp" /> <img src="../image/runes/Desintegrateur_de_sbires.webp" /> <img src="../image/runes/Livraison_de_biscuits.webp" /></th>
+    </tr>
+	
+	
+    <tr>
+        <th><img src="../image/runes/Coup_de_grace.webp" /> <img src="../image/runes/Abattage.webp" /> <img src="../image/runes/Baroud_d_honneur.webp" /></th>
+        <th><img src="../image/runes/Chasseur_de_tresor.webp" /> <img src="../image/runes/Chasseur_ingenieux.webp" /> <img src="../image/runes/Chasseur_acharne.webp" /> <img src="../image/runes/Chasseur_ultime.webp" /></th>
+        <th><img src="../image/runes/Brulure.webp" /> <img src="../image/runes/Marche_sur_l_eau.webp" /> <img src="../image/runes/Tempete_menacante.webp" /></th>
+		<th><img src="../image/runes/Surcroissance.webp" /> <img src="../image/runes/Revitalisation.webp" /> <img src="../image/runes/Inebranlable.webp" /></th>
+        <th><img src="../image/runes/Savoir_cosmique.webp" /> <img src="../image/runes/Vitesse_d_approche.webp" /> <img src="../image/runes/Philtre_de_chronodistortion.webp" /></th>
+    </tr>
 </table>
 
 <footer>
