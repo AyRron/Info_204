9ff8101ff5f44a3807bb8a01554138b2a02b6541
AyRron <cyrilpitar@orange.fr>
2022-05-18 14:12:38 +0200

    Update Item.php

--- INFO204/lib/Item.php
+++ INFO204/lib/Item.php
@@ -21,11 +21,11 @@
         <th><img class="combattant" src="../image/items/combattant/Force_de_la_trinite.webp" /></th>
     </tr>
     <tr>
-        <th><p>Eclipse</p></th>
-        <th><p>Estropieur</p></th>
-        <th><p>Eviscerateur</p></th>
-        <th><p>Pourfendeur divin</p></th>
-        <th><p>Force de la trinité</p></th>
+    <td><p>Éclipse</p></td>
+        <td><p>Éstropieur</p></td>
+        <td><p>Éviscerateur</p></td>
+        <td><p>Pourfendeur divin</p></td>
+        <td><p>Force de la trinité</p></td>
     </tr>
 </table>
 
@@ -50,22 +50,22 @@
         <th><img class="combattant" src="../image/items/combattant/Hydre_titanesque.webp" /></th>
     </tr>
     <tr>
-        <th><p>Lame enragée de Guinsoo</p></th>
-        <th><p>Epée dentelée chimico-punk</p></th>
-        <th><p>Brise coques</p></th>
-        <th><p>Ange gardien</p></th>
-        <th><p>Manamune</p></th>
-        <th><p>Gueule de Malmortius</p></th>
-        <th><p>Plaque du mort</p></th>
-        <th><p>Aube de Haut-Lac-d'Argent</p></th>
-        <th><p>Au bout du rouleau</p></th>
-        <th><p>Couperet noir</p></th>
-        <th><p>Gage de Sterak</p></th>
-        <th><p>Rancune de Serylda</p></th>
-        <th><p>Lame du roi déchu</p></th>
-        <th><p>Hydre vorace</p></th>
-        <th><p>Danse de la mort</p></th>
-        <th><p>Hydre titanesque</p></th>
+        <td><p>Lame enragée de Guinsoo</p></td>
+        <td><p>Épée dentelée chimico-punk</p></td>
+        <td><p>Brise coques</p></td>
+        <td><p>Ange gardien</p></td>
+        <td><p>Manamune</p></td>
+        <td><p>Gueule de Malmortius</p></td>
+        <td><p>Plaque du mort</p></td>
+        <td><p>Aube de Haut-Lac-d'Argent</p></td>
+        <td><p>Au bout du rouleau</p></td>
+        <td><p>Couperet noir</p></td>
+        <td><p>Gage de Sterak</p></td>
+        <td><p>Rancune de Serylda</p></td>
+        <td><p>Lame du roi déchu</p></td>
+        <td><p>Hydre vorace</p></td>
+        <td><p>Danse de la mort</p></td>
+        <td><p>Hydre titanesque</p></td>
     </tr>
 </table>
 
@@ -81,9 +81,9 @@
         <th><img class="tireur" src="../image/items/tireur/Arc-bouclier_immortel.webp" /></th>
     </tr>
     <tr>
-        <th><p>Tueur de krakens</p></th>
-        <th><p>Force du vent</p></th>
-        <th><p>Arc-bouclier immortel</p></th>
+        <td><p>Tueur de krakens</p></td>
+        <td><p>Force du vent</p></td>
+        <td><p>Arc-bouclier immortel</p></td>
     </tr>
 </table>
 
@@ -112,24 +112,24 @@
         <th><img class="tireur" src="../image/items/tireur/Soif-de-sang.webp" /></th>
     </tr>
     <tr>    
-        <th><p>Canon ultrarapide</p></th>
-        <th><p>Rappel mortel</p></th>
-        <th><p>Lame enragée de Guinsoo</p></th>
-        <th><p>Ouragan de Runaan</p></th>
-        <th><p>Danseur fantôme</p></th>
-        <th><p>Lame tempête</p></th>
-        <th><p>Faux spectrale</p></th>
-        <th><p>Ange gardien</p></th>
-        <th><p>Manamune</p></th>
-        <th><p>Gueule de Malmortius</p></th>
-        <th><p>Percepteur</p></th>
-        <th><p>Cimeterre mercuriel</p></th>
-        <th><p>Salutations de Dominik</p></th>
-        <th><p>Rancune de Serylda</p></th>
-        <th><p>Lame du roi déchu</p></th>
-        <th><p>Lame d'infini</p></th>
-        <th><p>Prestelames navori</p></th>
-        <th><p>Soif-de-sang</p></th>
+        <td><p>Canon ultrarapide</p></td>
+        <td><p>Rappel mortel</p></td>
+        <td><p>Lame enragée de Guinsoo</p></td>
+        <td><p>Ouragan de Runaan</p></td>
+        <td><p>Danseur fantôme</p></td>
+        <td><p>Lame tempête</p></td>
+        <td><p>Faux spectrale</p></td>
+        <td><p>Ange gardien</p></td>
+        <td><p>Manamune</p></td>
+        <td><p>Gueule de Malmortius</p></td>
+        <td><p>Percepteur</p></td>
+        <td><p>Cimeterre mercuriel</p></td>
+        <td><p>Salutations de Dominik</p></td>
+        <td><p>Rancune de Serylda</p></td>
+        <td><p>Lame du roi déchu</p></td>
+        <td><p>Lame d'infini</p></td>
+        <td><p>Prestelames navori</p></td>
+        <td><p>Soif-de-sang</p></td>
     </tr>
 </table>
 
@@ -138,27 +138,54 @@
 <h3> Items mythiques </h3>
 
 <table class="mythique">
-	<img class="assassin" src="../image/items/assassin/Epee_vesperale_de_Draktharr.webp" />
-	<img class="assassin" src="../image/items/assassin/Griffes_du_rodeur.webp" />
-	<img class="assassin" src="../image/items/assassin/Eclipse.webp" />
+    <tr>
+        <th><img class="assassin" src="../image/items/assassin/Epee_vesperale_de_Draktharr.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Griffes_du_rodeur.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Eclipse.webp" /></th>
+    </tr>
+    <tr>
+        <td><p>Épée vesperale de Draktharr</p></td>
+        <td><p>Griffes du rodeur</p></td>
+        <td><p>Éclipse</p></td>
+    </tr>
 </table>
+
 <h3> Items légendaires </h3>
 
+
 <table class="legendaire">
-	<img class="assassin" src="../image/items/assassin/Glaive_d_ombre.webp" />
-	<img class="assassin" src="../image/items/assassin/Crochet_de_serpent.webp" />
-	<img class="assassin" src="../image/items/assassin/Epee_dentelee_chimico-punk.webp" />
-	<img class="assassin" src="../image/items/assassin/Ange_gardien.webp" />
-	<img class="assassin" src="../image/items/assassin/Manamune.webp" />
-	<img class="assassin" src="../image/items/assassin/Manteau_de_la_nuit.webp" />
-	<img class="assassin" src="../image/items/assassin/Gueule_de_Malmortius.webp" />
-	<img class="assassin" src="../image/items/assassin/Lame_spectre_de_Youmuu.webp" />
-	<img class="assassin" src="../image/items/assassin/Percepteur.webp" />
-	<img class="assassin" src="../image/items/assassin/Arc_axiomatique.webp" />
-	<img class="assassin" src="../image/items/assassin/Aube_de_Haut-Lac-d_Argent.webp" />
-	<img class="assassin" src="../image/items/assassin/Couperet_noir.webp" />
-	<img class="assassin" src="../image/items/assassin/Rancune_de_Serylda.webp" />
-	<img class="assassin" src="../image/items/assassin/Hydre_vorace.webp" />
+    <tr>
+        <th><img class="assassin" src="../image/items/assassin/Glaive_d_ombre.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Crochet_de_serpent.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Epee_dentelee_chimico-punk.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Ange_gardien.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Manamune.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Manteau_de_la_nuit.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Gueule_de_Malmortius.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Lame_spectre_de_Youmuu.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Percepteur.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Arc_axiomatique.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Aube_de_Haut-Lac-d_Argent.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Couperet_noir.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Rancune_de_Serylda.webp" /></th>
+        <th><img class="assassin" src="../image/items/assassin/Hydre_vorace.webp" /></th>
+    </tr>
+    <tr>
+        <td><p>Glaive d'ombre</p></td>
+        <td><p>Crochet de serpent</p></td>
+        <td><p>Épée dentelée chimico-punk</p></td>
+        <td><p>Ange gardien</p></td>
+        <td><p>Manamune</p></td>
+        <td><p>Manteau de la nuit</p></td>
+        <td><p>Gueule de Malmortius</p></td>
+        <td><p>Lame spectre de Youmuu</p></td>
+        <td><p>Percepteur</p></td>
+        <td><p>Arc axiomatique</p></td>
+        <td><p>Aube de Haut-Lac-d'Argent</p></td>
+        <td><p>Couperet noir</p></td>
+        <td><p>Rancune de Serylda</p></td>
+        <td><p>Hydre vorace</p></td>
+    </tr>
 </table>
 
 
@@ -167,32 +194,61 @@
 <h3> Items mythiques </h3>
 
 <table class="mythique">
-	<img class="mage" src="../image/items/mage/Gel_eternel.webp" />
-	<img class="mage" src="../image/items/mage/Couronne_de_la_Reine_brisee.webp" />
-	<img class="mage" src="../image/items/mage/Affliction_de_Liandry.webp" />
-	<img class="mage" src="../image/items/mage/Tempete_de_Luden.webp" />
-	<img class="mage" src="../image/items/mage/Moissonneur_nocturne.webp" />
-	<img class="mage" src="../image/items/mage/Ceinture-roquette_Hextech.webp" />
-	<img class="mage" src="../image/items/mage/Createur_de_failles.webp" />
+    <tr>
+        <th><img class="mage" src="../image/items/mage/Gel_eternel.webp" /></th>
+        <th><img class="mage" src="../image/items/mage/Couronne_de_la_Reine_brisee.webp" /></th>
+        <th><img class="mage" src="../image/items/mage/Affliction_de_Liandry.webp" /></th>
+        <th><img class="mage" src="../image/items/mage/Tempete_de_Luden.webp" /></th>
+        <th><img class="mage" src="../image/items/mage/Moissonneur_nocturne.webp" /></th>
+        <th><img class="mage" src="../image/items/mage/Ceinture-roquette_Hextech.webp" /></th>
+        <th><img class="mage" src="../image/items/mage/Createur_de_failles.webp" /></th>
+    </tr>
+    <tr>
+        <td><p>Gel éternel</p></td>
+        <td><p>Couronne de la Reine brisée</p></td>
+        <td><p>Affliction de Liandry</p></td>
+        <td><p>Tempête de Luden</p></td>
+        <td><p>Moissonneur nocturne</p></td>
+        <td><p>Ceinture-roquette Hextech</p></td>
+        <td><p>Créateur de failles</p></td>
+    </tr>
 </table>
 
 <h3> Items légendaires </h3>
 
 <table class="legendaire">
-	<img class="mage" src="../image/items/mage/Voleur_d_ames_de_Mejai.webp" />
-	<img class="mage" src="../image/items/mage/Morellonomicon.webp" />
-	<img class="mage" src="../image/items/mage/Baton_de_l_archange.webp" />
-	<img class="mage" src="../image/items/mage/Voile_de_la_banshee.webp" />
-	<img class="mage" src="../image/items/mage/Sceptre_de_Rylai.webp" />
-	<img class="mage" src="../image/items/mage/Sablier_de_Zhonya.webp" />
-	<img class="mage" src="../image/items/mage/Baton_du_vide.webp" />
-	<img class="mage" src="../image/items/mage/Dent_de_Nashor.webp" />
-	<img class="mage" src="../image/items/mage/Flamme_ombre.webp" />
-	<img class="mage" src="../image/items/mage/Concentration_lointaine.webp" />
-	<img class="mage" src="../image/items/mage/Fleau_de_liche.webp" />
-	<img class="mage" src="../image/items/mage/Volonte_cosmique.webp" />
-	<img class="mage" src="../image/items/mage/Etreinte_demoniaque.webp" />
-	<img class="mage" src="../image/items/mage/Coiffe_de_Rabadon.webp" />
+    <tr>
+        <th><img class="mage" src="../image/items/mage/Voleur_d_ames_de_Mejai.webp" />
+        <th><img class="mage" src="../image/items/mage/Morellonomicon.webp" />
+        <th><img class="mage" src="../image/items/mage/Baton_de_l_archange.webp" />
+        <th><img class="mage" src="../image/items/mage/Voile_de_la_banshee.webp" />
+        <th><img class="mage" src="../image/items/mage/Sceptre_de_Rylai.webp" />
+        <th><img class="mage" src="../image/items/mage/Sablier_de_Zhonya.webp" />
+        <th><img class="mage" src="../image/items/mage/Baton_du_vide.webp" />
+        <th><img class="mage" src="../image/items/mage/Dent_de_Nashor.webp" />
+        <th><img class="mage" src="../image/items/mage/Flamme_ombre.webp" />
+        <th><img class="mage" src="../image/items/mage/Concentration_lointaine.webp" />
+        <th><img class="mage" src="../image/items/mage/Fleau_de_liche.webp" />
+        <th><img class="mage" src="../image/items/mage/Volonte_cosmique.webp" />
+        <th><img class="mage" src="../image/items/mage/Etreinte_demoniaque.webp" />
+        <th><img class="mage" src="../image/items/mage/Coiffe_de_Rabadon.webp" />
+    </tr>
+    <tr>
+        <td><p>Voleur d'ames de Mejai</p></td>
+        <td><p>Morellonomicon</p></td>
+        <td><p>Bâton de l'archange</p></td>
+        <td><p>Voile de la banshee</p></td>
+        <td><p>Sceptre de Rylai</p></td>
+        <td><p>Sablier de Zhonya</p></td>
+        <td><p>Bâton du vide</p></td>
+        <td><p>Dent de Nashor</p></td>
+        <td><p>Flamme ombre</p></td>
+        <td><p>Concentration lointaine</p></td>
+        <td><p>Fléau de liche</p></td>
+        <td><p>Volonté cosmique</p></td>
+        <td><p>Étreinte démoniaque</p></td>
+        <td><p>Coiffe de Rabadon</p></td>
+    </tr>
 </table>
 
 
@@ -201,61 +257,92 @@
 <h3> Items mythiques </h3>
 
 <table class="mythique">
-	<img class="tank" src="../image/items/tank/Linceul_de_la_congruence.webp" />
-	<img class="tank" src="../image/items/tank/Medaillon_de_l_Iron_Solari.webp" />
-	<img class="tank" src="../image/items/tank/Gantelet_cryopyrique.webp" />
-	<img class="tank" src="../image/items/tank/Chimico-scaphandre_turbo.webp" />
-	<img class="tank" src="../image/items/tank/Egide_solaire.webp" />
+    <tr>
+        <th><img class="tank" src="../image/items/tank/Linceul_de_la_congruence.webp" /></th>
+        <th><img class="tank" src="../image/items/tank/Medaillon_de_l_Iron_Solari.webp" /></th>
+        <th><img class="tank" src="../image/items/tank/Gantelet_cryopyrique.webp" /></th>
+        <th><img class="tank" src="../image/items/tank/Chimico-scaphandre_turbo.webp" /></th>
+        <th><img class="tank" src="../image/items/tank/Egide_solaire.webp" /></th>
+    </tr>
+    <tr>
+        <td><p>Linceul de la congruence</p></td>
+        <td><p>Médaillon de l'Iron Solari</p></td>
+        <td><p>Gantelet cryopyrique</p></td>
+        <td><p>Chimico-scaphandre turbo</p></td>
+        <td><p>Égide solaire</p></td>
+    </tr>
 </table>
+
 
 <h3> Items légendaires </h3>
 
-<table class="legendaire">
-	<img class="tank" src="../image/items/tank/Voeu_du_chevalier.webp" />
-	<img class="tank" src="../image/items/tank/Convergence_de_Zeke.webp" />
-	<img class="tank" src="../image/items/tank/Coeur_gele.webp" />
-	<img class="tank" src="../image/items/tank/Chaines_de_l_anatheme.webp" />
-	<img class="tank" src="../image/items/tank/Approche_de_l_hiver.webp" />
-	<img class="tank" src="../image/items/tank/Masque_abyssal.webp" />
-	<img class="tank" src="../image/items/tank/Presage_de_Randuin.webp" />
-	<img class="tank" src="../image/items/tank/Cotte_epineuse.webp" />
-	<img class="tank" src="../image/items/tank/Visage_spirituel.webp" />
-	<img class="tank" src="../image/items/tank/Force_de_la_nature.webp" />
-	<img class="tank" src="../image/items/tank/Plaque_du_mort.webp" />
-	<img class="tank" src="../image/items/tank/Etreinte_demoniaque.webp" />
-	<img class="tank" src="../image/items/tank/Armure_de_Warmog.webp" />
-	<img class="tank" src="../image/items/tank/Gage_de_Sterak.webp" />
-	<img class="tank" src="../image/items/tank/Lithoplastron_de_gargouille.webp" />
-	<img class="tank" src="../image/items/tank/Hydre_titanesque.webp" />
-</table>
 
+
+
+<table class="legendaire">
+    <tr>
+        <img class="tank" src="../image/items/tank/Voeu_du_chevalier.webp" />
+        <img class="tank" src="../image/items/tank/Convergence_de_Zeke.webp" />
+        <img class="tank" src="../image/items/tank/Coeur_gele.webp" />
+        <img class="tank" src="../image/items/tank/Chaines_de_l_anatheme.webp" />
+        <img class="tank" src="../image/items/tank/Approche_de_l_hiver.webp" />
+        <img class="tank" src="../image/items/tank/Masque_abyssal.webp" />
+        <img class="tank" src="../image/items/tank/Presage_de_Randuin.webp" />
+        <img class="tank" src="../image/items/tank/Cotte_epineuse.webp" />
+        <img class="tank" src="../image/items/tank/Visage_spirituel.webp" />
+        <img class="tank" src="../image/items/tank/Force_de_la_nature.webp" />
+        <img class="tank" src="../image/items/tank/Plaque_du_mort.webp" />
+        <img class="tank" src="../image/items/tank/Etreinte_demoniaque.webp" />
+        <img class="tank" src="../image/items/tank/Armure_de_Warmog.webp" />
+        <img class="tank" src="../image/items/tank/Gage_de_Sterak.webp" />
+        <img class="tank" src="../image/items/tank/Lithoplastron_de_gargouille.webp" />
+        <img class="tank" src="../image/items/tank/Hydre_titanesque.webp" />
+    </tr>
+    <tr>
+    <td><p>Vœu du chevalier</p></td>
+    <td><p>Convergence de Zéké</p></td>
+    <td><p>Cœr gelé</p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    <td><p></p></td>
+    </tr>
 
 <h2 class="support"> Items supports:</h2>
 <br>
 <h3> Items mythiques </h3>
 
 <table class="mythique">
-	<img class="support" src="../image/items/support/Chant_de_guerre_de_Shurelya.webp" />
-	<img class="support" src="../image/items/support/Mandat_imperial.webp" />
-	<img class="support" src="../image/items/support/Regenerateur_de_pierre_de_lune.webp" />
-	<img class="support" src="../image/items/support/Linceul_de_la_congruence.webp" />
-	<img class="support" src="../image/items/support/Medaillon_de_l_Iron_Solari.webp" />
-	<img class="support" src="../image/items/support/Chimico-scaphandre_turbo.webp" />
+<img class="support" src="../image/items/support/Chant_de_guerre_de_Shurelya.webp" />
+<img class="support" src="../image/items/support/Mandat_imperial.webp" />
+<img class="support" src="../image/items/support/Regenerateur_de_pierre_de_lune.webp" />
+<img class="support" src="../image/items/support/Linceul_de_la_congruence.webp" />
+<img class="support" src="../image/items/support/Medaillon_de_l_Iron_Solari.webp" />
+<img class="support" src="../image/items/support/Chimico-scaphandre_turbo.webp" />
 </table>
 
 <h3> Items légendaires </h3>
 
-<table class="legendaire"
-	<img class="support" src="../image/items/support/Encensoir_ardent.webp" />
-	<img class="support" src="../image/items/support/Putrificateur_techno-chimique.webp" />
-	<img class="support" src="../image/items/support/Baton_des_flots.webp" />
-	<img class="support" src="../image/items/support/Benediction_de_Mikael.webp" />
-	<img class="support" src="../image/items/support/Redemption.webp" />
-	<img class="support" src="../image/items/support/Voeu_du_chevalier.webp" />
-	<img class="support" src="../image/items/support/Convergence_de_Zeke.webp" />
-	<img class="support" src="../image/items/support/Approche_de_l_hiver.webp" />
-	<img class="support" src="../image/items/support/Masque_abyssal.webp" />
-</table>
+<table class="legendaire">
+<img class="support" src="../image/items/support/Encensoir_ardent.webp" />
+<img class="support" src="../image/items/support/Putrificateur_techno-chimique.webp" />
+<img class="support" src="../image/items/support/Baton_des_flots.webp" />
+<img class="support" src="../image/items/support/Benediction_de_Mikael.webp" />
+<img class="support" src="../image/items/support/Redemption.webp" />
+<img class="support" src="../image/items/support/Voeu_du_chevalier.webp" />
+<img class="support" src="../image/items/support/Convergence_de_Zeke.webp" />
+<img class="support" src="../image/items/support/Approche_de_l_hiver.webp" />
+<img class="support" src="../image/items/support/Masque_abyssal.webp" />
+
 
 </body>
 
